10ba566e9494e690ad82527e8140228627d499a3
Eugene Cheshenkov <e.cheshenkov@gmail.com>
2024-12-15 15:44:47 +0300

    Add Screen data generic and Navigation

--- GameSdk.Packages/GameSdk.UI/Assets/Package/Runtime/Navigation/Screen.cs
+++ GameSdk.Packages/GameSdk.UI/Assets/Package/Runtime/Navigation/Screen.cs
@@ -6,7 +6,8 @@ namespace GameSdk.UI
     [UxmlElement("Screen")]
     public abstract partial class Screen : VisualElement
     {
-        public Screen Parent { get; private set; }
+        public Screen Parent { get; protected set; }
+        public Navigation Navigation { get; protected set; }
 
         public event Action<AttachToPanelEvent, Screen> Attached;
 
@@ -18,6 +19,7 @@ namespace GameSdk.UI
 
         private void OnAttachToPanel(AttachToPanelEvent evt)
         {
+            Navigation = GetFirstAncestorOfType<Navigation>();
             Parent = GetFirstAncestorOfType<Screen>();
 
             Attached?.Invoke(evt, this);
@@ -26,6 +28,7 @@ namespace GameSdk.UI
         private void OnDetachFromPanel(DetachFromPanelEvent evt)
         {
             Parent = null;
+            Navigation = null;
         }
 
         internal void SetData(object data)
@@ -33,6 +36,28 @@ namespace GameSdk.UI
             OnDataChanged(data);
         }
 
-        protected virtual void OnDataChanged(object data) { }
+        protected virtual void OnDataChanged(object data)
+        {
+        }
+    }
+
+    public abstract partial class Screen<T> : Screen
+    {
+        public T Data { get; protected set; }
+
+        protected override void OnDataChanged(object data)
+        {
+            if (data is T typedData)
+            {
+                Data = typedData;
+                OnDataChanged();
+
+                return;
+            }
+
+            throw new InvalidCastException($"Cannot cast {data.GetType()} to {typeof(T)}");
+        }
+
+        protected abstract void OnDataChanged();
     }
 }
