74ed45e26bf039c0a6827860c503aa467f3f67a2
Eugene Cheshenkov <e.cheshenkov@gmail.com>
2025-02-27 21:26:12 +0300

    Add Editor extensions

---
+++ GameSdk.Packages/GameSdk.Core/Assets/Package/Runtime/Toolbox/Extensions/EditorExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace GameSdk.Core.Toolbox
+{
+    public static class EditorExtensions
+    {
+        public static IEnumerable<T> GetAssets<T>() where T : Object
+        {
+#if UNITY_EDITOR
+            var assets = UnityEditor.AssetDatabase.FindAssets(" t: " + typeof(T).Name);
+
+            return assets.Select(UnityEditor.AssetDatabase.GUIDToAssetPath)
+                         .Select(UnityEditor.AssetDatabase.LoadAssetAtPath<T>);
+#endif
+            return Enumerable.Empty<T>();
+        }
+    }
+}
