f434d799371eb8b8eba0aabbdf7d2ef6e6df1364
Eugene Cheshenkov <e.cheshenkov@gmail.com>
2025-08-14 22:41:06 +0300

    Add DataSourceButton class and update package versions

---
+++ GameSdk.Packages/GameSdk.UI/Assets/Package/Runtime/DataSource/DataSourceButton.cs
@@ -0,0 +1,43 @@
+using UnityEngine.Scripting;
+using UnityEngine.UIElements;
+
+[Preserve]
+public abstract partial class DataSourceButton : Button
+{
+    private IVisualElementScheduledItem _schedule;
+
+    [Preserve]
+    public DataSourceButton()
+    {
+        RegisterCallback<AttachToPanelEvent>(OnAttachToPanel);
+        RegisterCallback<DetachFromPanelEvent>(OnDetachFromPanel);
+    }
+
+    private void OnAttachToPanel(AttachToPanelEvent evt)
+    {
+        _schedule?.Pause();
+        _schedule = schedule.Execute(WaitDataSource).Every(10);
+    }
+
+    private void OnDetachFromPanel(DetachFromPanelEvent evt)
+    {
+        _schedule?.Pause();
+        _schedule = null;
+    }
+
+    private void WaitDataSource()
+    {
+        if (this.FindResolver() != null)
+        {
+            _schedule?.Pause();
+            _schedule = null;
+
+            OnDataSourceResolved();
+        }
+    }
+
+    protected virtual void OnDataSourceResolved()
+    {
+
+    }
+}
